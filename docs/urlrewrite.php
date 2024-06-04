3a3658c8d7c5dd4fb95b5fdfcce8e85fabce64e5
azikosan1997 <114338608+azikosan1997@users.noreply.github.com>
2024-06-04 08:52:59 +0500

    Chekpoint before adding news component

---
+++ docs/urlrewrite.php
@@ -0,0 +1,27 @@
+<?php
+$arUrlRewrite=array (
+  9 => 
+  array (
+    'CONDITION' => '#^/tj/o-banke/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/tj/o-banke/news/index.php',
+    'SORT' => 100,
+  ),
+  10 => 
+  array (
+    'CONDITION' => '#^/en/o-banke/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/en/o-banke/news/index.php',
+    'SORT' => 100,
+  ),
+  11 => 
+  array (
+    'CONDITION' => '#^/o-banke/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/o-banke/news/index.php',
+    'SORT' => 100,
+  ),
+);
